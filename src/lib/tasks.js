ecb5bacb32521226661647575bcb1efe997c2227
alan bount <alan@zeroasterisk.com>
2016-02-16 00:15:37 -0500

    react tutorial step3

--- src/lib/tasks.js
+++ src/lib/tasks.js
@@ -3,6 +3,7 @@ Tasks = new Mongo.Collection('tasks');
 
 Tasks.allow({
   insert: function (userId, doc) {
+    return true; // alan puts the cart before the horse
     // the user must be logged in, and the document must be owned by the user
     return (userId && doc.owner === userId);
   },
