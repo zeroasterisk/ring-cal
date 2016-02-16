8a77573231eb120583742cbbfc892239343619ee
alan bount <alan@zeroasterisk.com>
2016-02-16 00:09:04 -0500

    react tutorial step2

---
+++ src/lib/tasks.js
@@ -0,0 +1,18 @@
+// Define a collection to hold our tasks
+Tasks = new Mongo.Collection('tasks');
+
+Tasks.allow({
+  insert: function (userId, doc) {
+    // the user must be logged in, and the document must be owned by the user
+    return (userId && doc.owner === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    // can only change your own documents
+    return doc.owner === userId;
+  },
+  remove: function (userId, doc) {
+    // can only remove your own documents
+    return doc.owner === userId;
+  },
+  fetch: ['owner']
+});
